ebe0183fed91c5f53a73e01cc2713e9e84c40a60
StefanStoynov <sstoynov87@gmail.com>
2022-01-31 21:07:24 +0200

    create a deleteById test method

--- src/test/java/com/example/hibernate/repository/CourseRepositoryTest.java
+++ src/test/java/com/example/hibernate/repository/CourseRepositoryTest.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,11 +19,18 @@ class CourseRepositoryTest {
     @Autowired
     CourseRepository repository;
 
+
     @Test
-    void contextLoads() {
+    void findById() {
         Course course = repository.findById(10001L);
         assertEquals("hibernate", course.getName());
-        logger.info("Test is running");
     }
 
+    @Test
+    @DirtiesContext
+    //@DirtiesContext is used to repair the data after the test is done
+    void deleteById() {
+        repository.deleteById(10002L);
+        assertNull(repository.findById(10002L));
+    }
 }

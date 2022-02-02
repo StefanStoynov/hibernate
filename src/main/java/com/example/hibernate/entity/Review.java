0dbda5c99f75ab6f220a48f991d9afc8d1204ca5
StefanStoynov <sstoynov87@gmail.com>
2022-02-02 21:10:24 +0200

    introduce Review entity

--- src/main/java/com/example/hibernate/entity/Review.java
+++ src/main/java/com/example/hibernate/entity/Review.java
@@ -1,7 +1,52 @@
 package com.example.hibernate.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue
     private Long id;
-    private int rating;
+
+    private String rating;
+
     private String description;
+
+    public Review() {
+    }
+
+    public Review(String rating, String description) {
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "\nReview{" +
+                "rating='" + rating + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,7 @@ insert into student(id, name) values (20003, 'Ivan');
 insert into passport(id, number) values (30001,'A111');
 insert into passport(id, number) values (30002,'A222');
 insert into passport(id, number) values (30003,'A333');
+
+insert into review(id, rating, description) values (40001, '5', 'Great Course');
+insert into review(id, rating, description) values (40002, '4', 'Wonderful Course');
+insert into review(id, rating, description) values (40003, '5', 'Awesome Course');

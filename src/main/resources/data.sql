27dbd8bb18832b174c064090de10ec04330af7e6
StefanStoynov <sstoynov87@gmail.com>
2022-02-02 21:04:46 +0200

    introduce Passport entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,7 @@ insert into course(id, name) values (10003, 'JDBC' );
 insert into student(id, name) values (20001, 'Stefan');
 insert into student(id, name) values (20002, 'Petar');
 insert into student(id, name) values (20003, 'Ivan');
+
+insert into passport(id, number) values (30001,'A111');
+insert into passport(id, number) values (30002,'A222');
+insert into passport(id, number) values (30003,'A333');

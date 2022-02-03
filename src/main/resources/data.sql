012e7e0bd21215cbb0ef1535625bbbf3de7f6a4e
StefanStoynov <sstoynov87@gmail.com>
2022-02-03 20:46:32 +0200

    introduce passport_id foreign key

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,13 +2,13 @@ insert into course(id, name) values (10001, 'hibernate' );
 insert into course(id, name) values (10002, 'JPA' );
 insert into course(id, name) values (10003, 'JDBC' );
 
-insert into student(id, name) values (20001, 'Stefan');
-insert into student(id, name) values (20002, 'Petar');
-insert into student(id, name) values (20003, 'Ivan');
-
 insert into passport(id, number) values (30001,'A111');
 insert into passport(id, number) values (30002,'A222');
 insert into passport(id, number) values (30003,'A333');
+
+insert into student(id, name, passport_id) values (20001, 'Stefan', 30001);
+insert into student(id, name, passport_id) values (20002, 'Petar', 30002);
+insert into student(id, name, passport_id) values (20003, 'Ivan', 30003);
 
 insert into review(id, rating, description) values (40001, '5', 'Great Course');
 insert into review(id, rating, description) values (40002, '4', 'Wonderful Course');
